4b61052f3dc8f4077b4199b47d7268f426fe03a4
Denis <denvasilev1982@icloud.com>
2022-12-18 14:01:28 -0800

    урок 3, цикл for

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -36,27 +36,47 @@
 
 // вид 4
 
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
-    while (i < count)
-    {
-        result = result + text;
-        i++;
-    }
-    return result;
-}
+//string Method4(int count, string text)
+//{
+//    int i = 0;
+//   string result = String.Empty;
+//    while (i < count)
+//    {
+//        result = result + text;
+//        i++;
+//    }
+//    return result;
+//}
 
-string res = Method4(10, "z");
-Console.WriteLine(res);
-
-
+//string res = Method4(10, "z");
+//Console.WriteLine(res);
 
 
 
+//string Method4(int count, string text)
+//{
+//    string result = String.Empty;
+//    for (int i = 0; i < count; i++)
+//    {
+//    result = result + text;
+//}
+//return result;
+//}
+
+//string res = Method4(10, "z");
+//Console.WriteLine(res);
 
 
 
+
+
+
+
+
+
+
+
+
+ 
 
 
