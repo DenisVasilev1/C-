4715f02e61e7d845456573124916863901cb2231
Denis <denvasilev1982@icloud.com>
2022-12-18 14:31:08 -0800

    урок 3, работа с текстом

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -66,6 +66,48 @@
 //string res = Method4(10, "z");
 //Console.WriteLine(res);
 
+//for (int i = 2; i <= 10; i++)
+//{
+//    for (int j = 0; j <= 10; j++)
+//    {
+//        Console.WriteLine($"{i}*{j}={i * j}");
+//   }
+//    Console.WriteLine();
+//}
+
+// Дан тект. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К", а большие
+// "С" заменить маленькими "с".
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             0123
+// s[3] //r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine( );
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
 
 
 
@@ -77,6 +119,28 @@
 
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
